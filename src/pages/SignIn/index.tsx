6c2cae1d7889635ee969f1e4118afea84b600e0f
Ygor Mattos <ygormattos.b@gmail.com>
2020-07-27 21:16:46 -0300

    add fonts

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
-import { Container } from './styles';
+import { Image } from 'react-native';
+
+import { Container, Title } from './styles';
+
+import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Image source={logoImg} />
+
+      <Title>Faça seu logon</Title>
+    </Container>
+  );
 };
 
 export default SignIn;
