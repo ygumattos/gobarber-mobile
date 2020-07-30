d70caec75ed65e6d9a0b6f75f8e32c98229913fa
Ygor Mattos <ygormattos.b@gmail.com>
2020-07-29 21:21:57 -0300

    Connect with api for create user

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -13,6 +13,8 @@ import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
+import api from '../../services/api';
+
 import getValidationErros from '../../utils/getValidationErros';
 
 import Input from '../../components/Input';
@@ -51,9 +53,9 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
 
-      // await api.post('/users', data);
+      await api.post('/users', data);
 
-      // history.push('/');
+      navigation.goBack();
 
       Alert.alert(
         'Cadastro realizado!',
